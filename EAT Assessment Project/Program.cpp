3c5f0f5de974ddb0040d72747d03c0e2f796c29e
TheMGLegends <matejovo34@gmail.com>
2024-03-18 08:45:31 +0000

    Implementing Program Class
    The class in which the showcase will be run on.

--- EAT Assessment Project/Program.cpp
+++ EAT Assessment Project/Program.cpp
@@ -1,1 +1,73 @@
 #include "Program.h"
+
+Program::Program() :
+	window{ nullptr },
+	renderer{ nullptr },
+	isRunning{ false }
+{
+}
+
+/// <summary>
+/// Clean up method during program termination
+/// </summary>
+void Program::Clean()
+{
+	// INFO: Destroys the SDL renderer & window
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	//IMG_Quit();
+
+	// INFO: Unloads the main SDL Library
+	SDL_Quit();
+
+	// INFO: Clean up the instance
+	if (instance != nullptr)
+	{
+		delete instance;
+		instance = nullptr;
+	}
+}
+
+/// <summary>
+/// Initializes the SDL Libraries as well as other things like textures and game objects
+/// </summary>
+/// <param name="WINDOW_TITLE">: The title of the SDL window</param>
+/// <param name="screenWidth">: The width of the SDL window</param>
+/// <param name="screenHeight">: The height of the SDL window</param>
+/// <returns></returns>
+bool Program::Initialize(const char* WINDOW_TITLE, int screenWidth, int screenHeight)
+{
+	return false;
+}
+
+/// <summary>
+/// Handles the users input
+/// </summary>
+void Program::HandleInput()
+{
+}
+
+/// <summary>
+/// Processes the events that occur during the execution of the program
+/// e.g. (Collision Events)
+/// </summary>
+void Program::ProcessEvents()
+{
+}
+
+/// <summary>
+/// Updates all logic associated with each game object that exists in the program
+/// </summary>
+/// <param name="dt">: The deltaTime value used for scaling things with time</param>
+void Program::Update(float dt)
+{
+}
+
+/// <summary>
+/// Calls the draw function of each game object and draws them on the screen
+/// </summary>
+void Program::Draw()
+{
+}
+
--- EAT Assessment Project/Program.h
+++ EAT Assessment Project/Program.h
@@ -1,5 +1,34 @@
 #pragma once
-class Program
+
+#include "Singleton.h"
+
+#include <SDL.h>
+
+class Program : public Singleton<Program>
 {
+	// INFO: Grants the base class access to the private and protected
+	// member variables and methods of the derived class, specifically
+	// the constructor
+	friend class Singleton<Program>;
+
+private:
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	bool isRunning;
+
+public:
+	void Clean() override;
+
+	bool Initialize(const char* WINDOW_TITLE, int screenWidth, int screenHeight);
+	void HandleInput();
+	void ProcessEvents();
+	void Update(float dt);
+	void Draw();
+
+	inline bool IsRunning() const { return isRunning; }
+
+private:
+	Program();
 };
 
