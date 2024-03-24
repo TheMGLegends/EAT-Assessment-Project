9f6ba654bed2eb1a8322002da5475cba32231c82
TheMGLegends <matejovo34@gmail.com>
2024-03-24 15:22:00 +0000

    Working on Collision Manager
    Will need to adjust functions in collider classes as well as move the color struct to asset manager rather than having it in the game class or maybe even moving it to its own .h

--- EAT Assessment Project/CollisionManager.cpp
+++ EAT Assessment Project/CollisionManager.cpp
@@ -1,6 +1,12 @@
 #include "CollisionManager.h"
 
+#include "MemoryLeakDetector.h"
+
 CollisionManager* Singleton<CollisionManager>::instance = nullptr;
+
+CollisionManager::CollisionManager()
+{
+}
 
 void CollisionManager::Clean()
 {
@@ -9,5 +15,41 @@ void CollisionManager::Clean()
 	{
 		delete instance;
 		instance = nullptr;
+	}
+}
+
+void CollisionManager::CheckCollisions()
+{
+	// INFO: Used for comparing every collider with every other
+	// collider in the list, excluding self-comparisons
+	for (int i = 0; i < colliders.size(); i++)
+	{
+		for (int j = i + 1; j < colliders.size(); j++)
+		{
+			// INFO: Circle & Circle
+			if (colliders[i]->GetColliderType() == ColliderType::Circle &&
+				colliders[j]->GetColliderType() == ColliderType::Circle)
+			{
+
+			}
+			// INFO: Rect & Rect
+			else if (colliders[i]->GetColliderType() == ColliderType::Rect &&
+				colliders[j]->GetColliderType() == ColliderType::Rect)
+			{
+
+			}
+			// INFO: Rect & Circle
+			else if (colliders[i]->GetColliderType() == ColliderType::Rect &&
+				colliders[j]->GetColliderType() == ColliderType::Circle)
+			{
+
+			}
+			// INFO: Circle & Rect
+			else if (colliders[i]->GetColliderType() == ColliderType::Circle &&
+				colliders[j]->GetColliderType() == ColliderType::Rect)
+			{
+
+			}
+		}
 	}
 }
--- EAT Assessment Project/CollisionManager.h
+++ EAT Assessment Project/CollisionManager.h
@@ -2,9 +2,29 @@
 
 #include "Singleton.h"
 
+#include "Collider.h"
+
+#include <vector>
+
 class CollisionManager : public Singleton<CollisionManager>
 {
+	// INFO: Grants the base class access to the private and protected
+	// member variables and methods of the derived class, specifically
+	// the constructor
+	friend class Singleton<CollisionManager>;
+
+private:
+	std::vector<Collider*> colliders;
+
 public:
+	/// <summary>
+	/// Clean up method during program termination
+	/// </summary>
 	void Clean() override;
+
+	void CheckCollisions();
+
+private:
+	CollisionManager();
 };
 
